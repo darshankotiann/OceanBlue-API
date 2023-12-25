92f8cce3ee7b3625b49a0e86c45f962ad16555df
Darshan <darshankotiann@gmail.com>
2023-12-25 15:12:10 +0530

    Populate

--- controller/spsRequestController.js
+++ controller/spsRequestController.js
@@ -17,7 +17,7 @@ const createService = async (req, res) => {
 
 const getServiceData = async (req, res) => {
     try {
-        const response = await SpsRequest.find().populate("vendorID");
+        const response = await SpsRequest.find().populate("vendorID").populate("orderID");
         if (!response) {
             res.status(401).json({ error: true, message: "Something Went Wrong", response: response })
         } else {
